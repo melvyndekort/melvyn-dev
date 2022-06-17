56952dd38a4e7474d3d01a7564985989b775b197
Melvyn de Kort <melvyn@mdekort.nl>
2022-06-17 16:07:13 +0200

    Include infrastructure in this repo

---
+++ terraform/providers.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = ">= 3.0"
+    }
+  }
+}
+
+provider "cloudflare" {
+  api_token = data.terraform_remote_state.cloudsetup.outputs.api_token_apex_melvyn_dev
+}
