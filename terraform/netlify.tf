093b82a5e4e8d1e23ea4f60f22d532a3c8359b94
Melvyn de Kort <melvyn@mdekort.nl>
2022-06-17 22:44:09 +0200

    Fix some settings

--- terraform/netlify.tf
+++ terraform/netlify.tf
@@ -2,9 +2,12 @@ resource "netlify_site" "melvyn-dev" {
   name = "melvyn-dev"
 
   repo {
-    repo_branch = "main"
-    dir         = "src"
-    provider    = "github"
-    repo_path   = "melvyndekort/melvyn-dev"
+    repo_branch   = "main"
+    dir           = "src"
+    provider      = "github"
+    repo_path     = "melvyndekort/melvyn-dev"
+    custom_domain = "melvyn.dev"
+    cdp_enabled   = true
+    build_image   = "focal"
   }
 }
