9f14fb6c0d6ff9488019c4f1a5795006a5a7da3b
Melvyn de Kort <melvyn@mdekort.nl>
2022-06-18 03:10:45 +0200

    Revert "Fix some settings"
    This reverts commit dbacdfafa5fa89398813e1cb63b9dfecdafbc065.

--- terraform/netlify.tf
+++ terraform/netlify.tf
@@ -2,12 +2,9 @@ resource "netlify_site" "melvyn-dev" {
   name = "melvyn-dev"
 
   repo {
-    repo_branch   = "main"
-    dir           = "src"
-    provider      = "github"
-    repo_path     = "melvyndekort/melvyn-dev"
-    custom_domain = "melvyn.dev"
-    cdp_enabled   = true
-    build_image   = "focal"
+    repo_branch = "main"
+    dir         = "src"
+    provider    = "github"
+    repo_path   = "melvyndekort/melvyn-dev"
   }
 }
