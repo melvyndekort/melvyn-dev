c8a027b8becbbb92a939daece464a14122ed9032
Melvyn de Kort <melvyn@mdekort.nl>
2022-07-21 12:16:24 +0200

    Use data resource instead of remote state to retrieve zone_id

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -1,5 +1,11 @@
+data "cloudflare_zones" "zone" {
+  filter {
+    name = "melvyn.dev"
+  }
+}
+
 resource "cloudflare_record" "apex" {
-  zone_id = data.terraform_remote_state.cloudsetup.outputs.melvyn_dev_zone_id
+  zone_id = data.cloudflare_zones.zone.zones[0].id
   name    = var.domain_name
   type    = "CNAME"
   ttl     = 1
@@ -10,7 +16,7 @@ resource "cloudflare_record" "apex" {
 resource "cloudflare_record" "aliasses" {
   for_each = toset(var.domain_aliasses)
 
-  zone_id = data.terraform_remote_state.cloudsetup.outputs.melvyn_dev_zone_id
+  zone_id = data.cloudflare_zones.zone.zones[0].id
   name    = each.key
   type    = "CNAME"
   ttl     = 1
